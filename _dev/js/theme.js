3920f3a1254f3f4bb0e7e6189f29c50c95ce7eba
L'huillier Thomas <tlhuillier@202-ecommerce.com>
2018-09-22 22:54:13 +0200

    Add browserSync

--- _dev/js/theme.js
+++ _dev/js/theme.js
@@ -5,27 +5,29 @@
  * This comment should be preserved in production mode.
  */
 
+import '../css/theme.scss';
+
 /**
  * This one will we removed.
  */
 
-if (true && false) {
-  // webpack will drop this unreachable code
-  console.log('useless code here');
-  new Human('R2-D2');
-}
-
-console.log('This console log will be dropped in production mode.');
-
-new Human('E-3PO');
-
 class Droid {
   constructor(name = undefined) {
     this.name = name;
-    this.say('Hi');
+    this.say(`Hi, I'm ${this.name}.\nWhat can I do for you?`);
   }
 
   say(str) {
     console.log(str);
   }
 }
+
+if (true && false) {
+  // webpack will drop this unreachable code
+  console.log('useless code here');
+  let d1 = new Droid('R2-D2');
+}
+
+console.log('This console log will be dropped in production mode.');
+
+let d2 = new Droid('E-3PO');
--- _dev/webpack.config.js
+++ _dev/webpack.config.js
@@ -1,42 +1,25 @@
-/**
- * 2007-2017 PrestaShop
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Academic Free License 3.0 (AFL-3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * https://opensource.org/licenses/AFL-3.0
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@prestashop.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
- * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to http://www.prestashop.com for more information.
- *
- * @author    PrestaShop SA <contact@prestashop.com>
- * @copyright 2007-2017 PrestaShop SA
- * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License 3.0 (AFL-3.0)
- * International Registered Trademark & Property of PrestaShop SA
- */
-
 const path = require('path');
+const webpack = require('webpack');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const minimizers = [];
 const plugins = [
-  new MiniCssExtractPlugin({
-    filename: '../css/theme.css'
+  new BrowserSyncPlugin({
+    host: 'localhost',
+    port: 3100,
+    proxy: 'http://ps1742.thomas.tot/'
   }),
+  new webpack.HotModuleReplacementPlugin(),
   // Clear the assets/img/generated folder before each compilation.
   new CleanWebpackPlugin(['assets/img/generated'], {
     root: path.resolve(__dirname, '..'),
     verbose: true
+  }),
+  new MiniCssExtractPlugin({
+    filename: '../css/theme.css'
   })
 ];
 
